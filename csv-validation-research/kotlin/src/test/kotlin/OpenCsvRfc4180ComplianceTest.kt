ba6b068309c9fa9ba07d37918eb8f6ac885775d0
Smith <ewl0@cdc.gov>
2024-03-19 11:02:01 -0400

    added placeholder unit tests for rfc-4180 compliance for openCSV ApacheCommons kotlin-csv UnivocityParser

---
+++ csv-validation-research/kotlin/src/test/kotlin/OpenCsvRfc4180ComplianceTest.kt
@@ -0,0 +1,57 @@
+import org.junit.jupiter.api.Test
+
+class OpenCsvRfc4180ComplianceTest {
+
+    @Test
+    fun lineBreakCRLF(){
+        //
+    }
+    @Test
+    fun lineBreakCR(){
+        //
+    }
+    @Test
+    fun lineBreakLF(){
+        //
+    }
+    @Test
+    fun lineBreakAtTheEndOfFile(){
+        //
+    }
+    @Test
+    fun noHeader(){
+        //
+    }
+    @Test
+    fun fieldOneOrMoreFields(){
+        //
+    }
+    @Test
+    fun recordWithDifferentNumberOfFields(){
+        //
+    }
+    @Test
+    fun recordFieldsWithSpaces(){
+        //
+    }
+    @Test
+    fun lastFieldInRecordFollowedByComma(){
+        //
+    }
+    @Test
+    fun quotesInFieldNotEnclosedWithDoubleQuotes(){
+        //
+    }
+    @Test
+    fun doubleQuotesInsideQuotedField(){
+        //
+    }
+    @Test
+    fun fieldsWithLineBreaksInsideQuotes(){
+        //
+    }
+    @Test
+    fun fieldsWithCommasInsideQuotes() {
+        //
+    }
+}
---
+++ csv-validation-research/kotlin/src/test/kotlin/UnivocityParserRfc4180ComplianceTestkt.kt
@@ -0,0 +1,56 @@
+import org.junit.jupiter.api.Test
+
+class UnivocityParserRfc4180ComplianceTestkt {
+    @Test
+    fun lineBreakCRLF(){
+        //
+    }
+    @Test
+    fun lineBreakCR(){
+        //
+    }
+    @Test
+    fun lineBreakLF(){
+        //
+    }
+    @Test
+    fun lineBreakAtTheEndOfFile(){
+        //
+    }
+    @Test
+    fun noHeader(){
+        //
+    }
+    @Test
+    fun fieldOneOrMoreFields(){
+        //
+    }
+    @Test
+    fun recordWithDifferentNumberOfFields(){
+        //
+    }
+    @Test
+    fun recordFieldsWithSpaces(){
+        //
+    }
+    @Test
+    fun lastFieldInRecordFollowedByComma(){
+        //
+    }
+    @Test
+    fun quotesInFieldNotEnclosedWithDoubleQuotes(){
+        //
+    }
+    @Test
+    fun doubleQuotesInsideQuotedField(){
+        //
+    }
+    @Test
+    fun fieldsWithLineBreaksInsideQuotes(){
+        //
+    }
+    @Test
+    fun fieldsWithCommasInsideQuotes() {
+        //
+    }
+}
