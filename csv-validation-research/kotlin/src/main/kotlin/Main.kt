c583b238d6bdd2d7204aa2544abf2c24782651fd
Smith <ewl0@cdc.gov>
2024-04-15 16:10:47 -0400

    updated existing csv libraries to iterate through the row and get field removed print statements added functions for Super CSV, Jackson CSV, and Fast CSV.

--- csv-validation-research/kotlin/src/main/kotlin/Main.kt
+++ csv-validation-research/kotlin/src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import com.fasterxml.jackson.dataformat.csv.CsvMapper
 import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
 import com.opencsv.CSVParserBuilder
 import com.opencsv.CSVReaderBuilder
@@ -5,30 +6,33 @@ import com.sun.jna.StringArray
 
 import com.univocity.parsers.csv.CsvParser
 import com.univocity.parsers.csv.CsvParserSettings
+import de.siegmar.fastcsv.reader.CsvRecord
 
 import io.deephaven.csv.CsvSpecs
-import io.deephaven.csv.reading.CsvReader
+import io.deephaven.csv.reading.CsvReader as DeephavenCSVReader
 import io.deephaven.csv.sinks.SinkFactory
 
+import de.siegmar.fastcsv.reader.CsvReader as FastCSVReader
 import org.apache.commons.csv.CSVFormat
 import org.apache.commons.csv.CSVParser
+import org.supercsv.io.CsvListReader
+import org.supercsv.prefs.CsvPreference
+
 
 import java.io.BufferedReader
 import java.io.File
 import java.io.InputStream
 import java.io.InputStreamReader
+import java.nio.file.Path
+import java.nio.file.Paths
 
 import java.time.LocalDateTime
-import java.time.temporal.ChronoUnit
-import java.util.*
+
 
 
 class CSVValidator {
 
     fun validateWithKotlinCSV(filename: String) {
-        val startTime = LocalDateTime.now()
-         val endTime: LocalDateTime?
-
         try {
             val resourceURL = object {}.javaClass.getResource(filename)
             val file: File? = resourceURL?.toURI()?.let { File(it) }
@@ -37,64 +41,43 @@ class CSVValidator {
                 csvReader().open(file) {
                     readAllAsSequence().forEach { row: List<String> ->
                         for (field in row){
-                            continue
+                            val f = field
                         }
                     }
                 }
             }
         }catch (e: Exception){
-            println (e.printStackTrace())
-        }finally {
-            endTime = LocalDateTime.now()
+            e.printStackTrace()
         }
-        val totalTime= ChronoUnit.MILLIS.between(startTime,endTime)
-        println ("Total processing time $filename took in milliseconds: $totalTime")
     }
-    fun validateWithOpenCSV(filename:String): List<String> {
+    fun validateWithOpenCSV(filename:String) {
 
-        val startTime = LocalDateTime.now()
-        val endTime: LocalDateTime?
-        val parsedData = mutableListOf<String>()
         try {
             val inputStream: InputStream? =
                 CSVValidator::class.java.getResourceAsStream("/$filename")
 
             val csvParserBuilder = CSVParserBuilder()
-                .withSeparator(',')
-                .withQuoteChar('"')
                 .withIgnoreLeadingWhiteSpace(false)
                 .withIgnoreQuotations(false)
-                .withEscapeChar('\r')
 
                 .build()
             val builder = CSVReaderBuilder(inputStream?.let { InputStreamReader(it) })
                 .withCSVParser(csvParserBuilder)
-                .withErrorLocale(Locale.US)
-                .withKeepCarriageReturn(true)
                 .build()
 
             var record: Array<String>?
             while (builder.readNext().also {record=it} !=null){
-                val row = record?.joinToString(",")
-                println (row)
-                if (row != null) {
-                    parsedData.add(row)
+                for (row in record!!){
+                    val field = row
                 }
             }
             builder.close()
         }catch (e: Exception) {
             e.printStackTrace()
-        }finally {
-            endTime = LocalDateTime.now()
         }
 
-        val totalTime= ChronoUnit.MILLIS.between(startTime,endTime)
-        println ("Total processing time $filename took in milliseconds: $totalTime")
-        return parsedData
     }
     fun validateWithApacheCommons(filename:String) {
-        val startTime = LocalDateTime.now()
-        val endTime: LocalDateTime?
         try {
             val inputStream: InputStream? =
                 CSVValidator::class.java.getResourceAsStream("/$filename")
@@ -102,32 +85,21 @@ class CSVValidator {
             val bufferedReader =  BufferedReader(inputStreamReader)
 
             val csvParser = CSVParser(bufferedReader,CSVFormat.RFC4180)
-            println (csvParser.records)
-
-
             for (csvRecord in csvParser){
-                println (csvRecord)
+                for (field in csvRecord){
+                    val f = field
+                }
             }
-
             inputStream.close()
             inputStreamReader.close()
             bufferedReader.close()
             csvParser.close()
         }catch (e: Exception) {
-            println ("Error parsing CSV: ${e.message}")
             e.printStackTrace()
-        }finally {
-            endTime = LocalDateTime.now()
         }
-
-        val totalTime= ChronoUnit.MILLIS.between(startTime,endTime)
-        println ("Total processing time $filename took in milliseconds: $totalTime")
-
     }
 
     fun univocityCSVParser(filename: String){
-        val startTime = LocalDateTime.now()
-        val endTime: LocalDateTime?
         try {
             val settings = CsvParserSettings()
             settings.detectFormatAutomatically()
@@ -143,11 +115,14 @@ class CSVValidator {
             parser.beginParsing(bufferedReader)
 
             var record: Array<String>?
-            while (parser.parseNext() !=null){
-                record = arrayOf(parser.context.currentParsedContent())
+            while (parser.parseNext()!=null){
 
-                for (row in record){
-                    println (row)
+                record = arrayOf(parser.context.currentParsedContent())
+                //convert record to csv row
+                val csvRecord = record.joinToString(",")
+                // get fields
+                for (field in csvRecord.split(",")){
+                    val f = field
                 }
             }
 
@@ -158,16 +133,10 @@ class CSVValidator {
             inputStream.close()
         }catch (e: Exception){
             e.printStackTrace()
-        }finally {
-            endTime = LocalDateTime.now()
         }
-        val totalTime= ChronoUnit.MILLIS.between(startTime,endTime)
-        println ("Total processing time $filename took  in milliseconds: $totalTime")
     }
 
     fun  deepHavenCSVParser(filename: String) {
-        val startTime = LocalDateTime.now()
-        val endTime: LocalDateTime?
         try {
             val inputStream = object {}.javaClass.getResourceAsStream(filename)
             val specs = CsvSpecs.builder()
@@ -177,62 +146,95 @@ class CSVValidator {
                 .delimiter(',')
                 .build()
 
-            val result = CsvReader.read(specs, inputStream, SinkFactory.arrays())
-
-
-            //val numOfRows = result.numRows() //returns number of rows
-
+            val result = DeephavenCSVReader.read(specs, inputStream, SinkFactory.arrays())
             for (col in result) {
-                println("The data type for ${col.name()} is ${col.dataType()}")
                 if (col.data() is Array<*>) {
-                    println("Column: ${col.name()} fields:")
                     for (element in col.data() as Array<*>) {
-                        println(element)
+                        val field = element
                     }
                 } else if (col.data() is IntArray) {
-                    println("Column: ${col.name()} fields:")
                     for (element in col.data() as IntArray) {
-                        println(element)
+                        val field = element
                     }
                 } else if (col.data() is StringArray) {
                     for (element in col.data() as String) {
-                        println(element)
+                        val field = element
                     }
                 }
             }
         }catch(e: Exception){
             e.printStackTrace()
-        }   finally {
-            endTime = LocalDateTime.now()
         }
-        val totalTime= ChronoUnit.MILLIS.between(startTime,endTime)
-        println ("Total processing time $filename took  in milliseconds: $totalTime")
+    }
+
+    fun fastCSVParser(filename: String){
+
+        val pathToCSV: Path? = CSVValidator::class.java.getResource("/$filename")?.toURI()?.let { Paths.get(it) }
+        try {
+            val reader = FastCSVReader.builder().ofCsvRecord(pathToCSV).iterator()
+            while (reader.hasNext()) {
+                val csvRecord: CsvRecord = reader.next()
+                for (field in csvRecord.fields){
+                    val f = field
+                }
+            }
+
+        } catch (e: Exception){
+            e.printStackTrace()
+        }
+
+    }
+    fun superCSVParser(filename: String) {
+        val csvInputSteam: InputStream? = CSVValidator::class.java.getResourceAsStream("/$filename")
+        val reader = csvInputSteam?.let { InputStreamReader(it) }
+        val csvReader = CsvListReader(reader, CsvPreference.STANDARD_PREFERENCE) // you can also configure with Tab
+
+        try {
+
+            var rowRecord: List<String>?
+            while (csvReader.read().also { rowRecord = it } != null) {
+                rowRecord?.let {
+                    for (field in rowRecord!!){
+                        val f = field
+                    }
+                }
+            }
+        } finally {
+            csvReader.close()
+            reader?.close()
+            csvInputSteam?.close()
+        }
+    }
+    fun jacksonCSVParser(filename: String){
+        val mapper = CsvMapper() // create mapper to provide reading or writing functionality
+
+        val csvFile = File("src/main/resources/$filename")
+        val iter = mapper
+            .readerForListOf(String::class.java)
+            .with(com.fasterxml.jackson.dataformat.csv.CsvParser.Feature.WRAP_AS_ARRAY)
+            .readValues<List<String>>(csvFile)// converts each row list of strings
+
+        while (iter.hasNext()) {
+            val row = iter.next()
+            for (field in row){
+                val f = field
+            }
+        }
     }
 }
 
 
-
 fun main(){
-    /*
-    to run with the profiler, you can surround each function logic with outer
-    for loop with number of iterations, then
     val startTime = LocalDateTime.now()
     val validate = CSVValidator()
-    validate.validateWithOpenCSV("file-with-headers-100-rows.csv")
-    val endTime: LocalDateTime?
-     */
-    val validate = CSVValidator()
+    repeat(1){
+        validate.superCSVParser("file-with-headers-100-rows.csv")
+    }
 
-    validate.validateWithOpenCSV("file-with-headers-100-rows.csv")
-    validate.univocityCSVParser("file-with-headers-100-rows.csv")
-    validate.deepHavenCSVParser("file-with-headers-100-rows.csv")
-    validate.validateWithKotlinCSV("file-with-headers-100-rows.csv")
-    validate.validateWithApacheCommons("file-with-headers-100-rows.csv")
-    validate.validateWithOpenCSV("file-with-headers-10000-rows.csv")
-    validate.univocityCSVParser("file-with-headers-10000-rows.csv")
-    validate.deepHavenCSVParser("file-with-headers-10000-rows.csv")
-    validate.validateWithKotlinCSV("file-with-headers-10000-rows.csv")
-    validate.validateWithApacheCommons("file-with-headers-10000-rows.csv")
+    val endTime = LocalDateTime.now()
+    val duration = java.time.Duration.between(startTime, endTime).toMillis()
 
+    println("The Total processing time: $duration milliseconds")
 
 }
+
