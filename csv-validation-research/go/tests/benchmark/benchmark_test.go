fb9aee4e7eb1fef8357269967af847a5867416ec
Erik Knudsen <31932266+erik1066@users.noreply.github.com>
2024-04-26 10:53:40 -0400

    Fixed broken Go benchmark

--- csv-validation-research/go/tests/benchmark/benchmark_test.go
+++ csv-validation-research/go/tests/benchmark/benchmark_test.go
@@ -15,37 +15,39 @@ type Row struct {
 
 func BenchmarkCSVValidator(b *testing.B) {
 	b.ReportAllocs() //report allocations during benchmark
-	f, err := os.Open("../../data/file-with-headers-10000-rows.csv")
+	for i := 0; i < b.N; i++ {
+		f, err := os.Open("../../data/file-with-headers-10000-rows.csv")
 
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
-	defer func(f *os.File) {
-		err := f.Close()
 		if err != nil {
-			log.Fatal(err)
+			fmt.Println("Error:", err)
 		}
-	}(f)
-	reader := csv.NewReader(f)
+		defer func(f *os.File) {
+			err := f.Close()
+			if err != nil {
+				log.Fatal(err)
+			}
+		}(f)
+		reader := csv.NewReader(f)
 
-	var rows []Row
-	for {
-		record, err := reader.Read()
-		if err == io.EOF {
-			break
+		var rows []Row
+		for {
+			record, err := reader.Read()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			//create row object
+			row := Row{Data: make(map[int]string)}
+			for i, field := range record {
+				row.Data[i] = field
+
+			}
+			rows = append(rows, row)
+
 		}
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		//create row object
-		row := Row{Data: make(map[int]string)}
-		for i, field := range record {
-			row.Data[i] = field
-
-		}
-		rows = append(rows, row)
-
 	}
 
 }
