94c72644ab1cf05617a5da8fdcfae15e382beb24
TeaSmith7 <137535421+TeaSmith7@users.noreply.github.com>
2024-05-29 18:33:50 -0400

    changes to the detect bom: reseting the file pointer to the begining of the file to so that same file pointer can be read from begining when passed to other functions for example to  detect_encoding. updated the logic of detecting BOM by comparing byte slice  and avoiding conversion to string. moved the UTF bom sequences from constants and made it package level constant.

--- internal/utils/detect_bom.go
+++ internal/utils/detect_bom.go
@@ -1,9 +1,13 @@
 package utils
 
 import (
-	"github.com/CDCgov/data-exchange-csv/cmd/internal/constants"
+	"bytes"
 	"io"
 	"os"
+)
+
+var (
+	UTF8_BOM = []byte{0xEF, 0xBB, 0xBF}
 )
 
 func DetectBOM(file *os.File) (bool, error) {
@@ -13,9 +17,14 @@ func DetectBOM(file *os.File) (bool, error) {
 		return false, err
 	}
 
-	// check if BOM is present and return true
-	if string(bom) == constants.UTF8BOMHex || string(bom) == constants.UTF8BOMUnicode {
+	//reset the file pointer to the begining of the file
+	if _, err := file.Seek(0, 0); err != nil {
+		return false, err
+	}
+	//compare the first three bytes of the file with the UTF-8 BOM sequence
+	if bytes.Equal(bom, UTF8_BOM) {
 		return true, nil
 	}
+
 	return false, nil
 }
