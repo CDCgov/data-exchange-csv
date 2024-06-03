dfc249c9417a687adfcaa27f2e709386f395fbec
Thomas Phung <81277197+thomasphung@users.noreply.github.com>
2024-06-03 15:50:34 -0700

    Moving main business logic out of main(); using slog for logging

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,31 @@
 package main
 
+// TODO: Move run() into a separate app package? That way we can organize how we define different execution envs - Thomas
 import (
+	"context"
 	"encoding/csv"
 	"fmt"
 	"io"
+	"log/slog"
 	"os"
+	"os/signal"
+	"strings"
 
 	"github.com/CDCgov/data-exchange-csv/cmd/internal/validate/file"
 	"golang.org/x/text/encoding/charmap"
 )
 
-func main() {
+func run(
+	ctx context.Context,
+	args []string,
+	stdout, stderr io.Writer,
+) error {
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
+	defer cancel()
+
+	logger := slog.New(slog.NewTextHandler(stdout, nil))
+	slog.SetDefault(logger)
+
 	//source := "data/file-with-headers-100-rows.csv"
 	//source := "data/file-with-headers-100-rows_with_BOM.csv"
 	//source := "data/file-with-headers-100-rows_US_ASCII.csv"
@@ -19,7 +34,9 @@ func main() {
 
 	fileValidationResult := file.Validate(source)
 
-	fmt.Println("file validation result: ", fileValidationResult)
+	// TODO: FileValidationResult will contain unescaped quotation marks when converted to a string, this will
+	// confuse any upstream consumers of logs b/c they are not readable in JSON
+	slog.Info("file validation result: ", fileValidationResult)
 
 	//detect encoding with random sample data
 	//enc := utils.DetectEncoding(randomSampleData)
@@ -28,7 +45,7 @@ func main() {
 	//tempcode-> check if windows1252 decoder can correctly parse the csv file
 	file, err := os.Open(source)
 	if err != nil {
-		fmt.Println(err)
+		slog.Error(err.Error())
 	}
 	reader := csv.NewReader(decoder.Reader(file))
 
@@ -37,6 +54,16 @@ func main() {
 		if err == io.EOF {
 			break
 		}
-		fmt.Println(record)
+		slog.Info(strings.Join(record, ""))
+	}
+
+	return nil
+}
+
+func main() {
+	ctx := context.Background()
+	if err := run(ctx, os.Args, os.Stdout, os.Stderr); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
 	}
 }
