5be78d33aedaf9c995d814755fa9a78901dde0c5
Thomas Phung <81277197+thomasphung@users.noreply.github.com>
2024-06-06 14:03:07 -0700

    Removed CSV business logic from main.go to focus on HTTP server implementation

--- cmd/main.go
+++ cmd/main.go
@@ -3,16 +3,12 @@ package main
 // TODO: Move run() into a separate app package? That way we can organize how we define different execution envs - Thomas
 import (
 	"context"
-	"encoding/csv"
 	"fmt"
+	"github.com/CDCgov/data-exchange-csv/cmd/internal/server"
 	"io"
 	"log/slog"
 	"os"
 	"os/signal"
-	"strings"
-
-	"github.com/CDCgov/data-exchange-csv/cmd/internal/validate/file"
-	"golang.org/x/text/encoding/charmap"
 )
 
 func run(
@@ -20,42 +16,19 @@ func run(
 	args []string,
 	stdout, stderr io.Writer,
 ) error {
+	// TODO: Add elapsed time since service starts to logging?
+	log := slog.New(slog.NewTextHandler(stdout, nil))
+	slog.SetDefault(log)
+
 	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
 	defer cancel()
 
-	logger := slog.New(slog.NewTextHandler(stdout, nil))
-	slog.SetDefault(logger)
+	log.Info("Starting DEX CSV Validation")
 
-	//source := "data/file-with-headers-100-rows.csv"
-	//source := "data/file-with-headers-100-rows_with_BOM.csv"
-	//source := "data/file-with-headers-100-rows_US_ASCII.csv"
-	//source := "data/file-with-headers-rows_iso8859-1.csv"
-	source := "data/file-with-headers-windows1252.csv"
+	// TODO: Add event loop
+	server.New()
 
-	fileValidationResult := file.Validate(source)
-
-	// TODO: FileValidationResult will contain unescaped quotation marks when converted to a string, this will
-	// confuse any upstream consumers of logs b/c they are not readable in JSON
-	slog.Info("file validation result: ", fileValidationResult)
-
-	//detect encoding with random sample data
-	//enc := utils.DetectEncoding(randomSampleData)
-	decoder := charmap.Windows1252.NewDecoder()
-
-	//tempcode-> check if windows1252 decoder can correctly parse the csv file
-	file, err := os.Open(source)
-	if err != nil {
-		slog.Error(err.Error())
-	}
-	reader := csv.NewReader(decoder.Reader(file))
-
-	for {
-		record, err := reader.Read()
-		if err == io.EOF {
-			break
-		}
-		slog.Info(strings.Join(record, ""))
-	}
+	log.Info("Exiting DEX CSV Validation")
 
 	return nil
 }
@@ -63,7 +36,7 @@ func run(
 func main() {
 	ctx := context.Background()
 	if err := run(ctx, os.Args, os.Stdout, os.Stderr); err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
+		_, _ = fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(1)
 	}
 }
