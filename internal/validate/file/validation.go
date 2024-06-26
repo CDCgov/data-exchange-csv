57d9fd86d08dfa8e64159bb9cfef5f853a536647
Thomas Phung <81277197+thomasphung@users.noreply.github.com>
2024-06-26 16:15:35 -0700

    Stripping package file to function signatures for testing HTTP server

--- internal/validate/file/validation.go
+++ internal/validate/file/validation.go
@@ -9,11 +9,7 @@ Key components contributing to the overall validation of a file include:
 package file
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/CDCgov/data-exchange-csv/cmd/internal/constants"
-	"github.com/CDCgov/data-exchange-csv/cmd/internal/utils"
 )
 
 type FileValidationResult struct {
@@ -28,50 +24,6 @@ type FileValidationResult struct {
 }
 
 func Validate(filePath string) FileValidationResult {
-	file, err := os.Open(filePath)
-	if err != nil {
-		return FileValidationResult{
-			Source: filePath,
-			Error:  err,
-		}
-	}
-	defer func() {
-		if err := file.Close(); err != nil {
-			fmt.Println(constants.FILE_CLOSE_ERROR)
-			return
-		}
-	}()
-
-	result := FileValidationResult{
-		Name:   file.Name(),
-		Source: filePath,
-	}
-
-	// Detect BOM
-	hasBOM, err := utils.DetectBOM(file)
-	if err != nil {
-		result.Error = err
-		return result
-	}
-
-	// Get a random sample of the file data
-	sampleData, err := utils.GetRandomSample(file)
-	if err != nil {
-		result.Error = err
-		return result
-	}
-
-	// Detect delimiter
-	delimiterDetector := &utils.DelimiterDetector{}
-	detectedDelimiter := delimiterDetector.DetectDelimiter(string(sampleData))
-	result.Delimiter = constants.DelimiterCharacters[detectedDelimiter.Character]
-
-	// Detect encoding
-	if hasBOM {
-		result.Encoding = constants.UTF8_BOM
-	} else {
-		result.Encoding = utils.DetectEncoding(sampleData)
-	}
-
+	result := FileValidationResult{}
 	return result
 }
