7e49b26730c0f149bdf7ca4415f066d5a02038ac
Thomas Phung <81277197+thomasphung@users.noreply.github.com>
2024-07-01 11:40:09 -0700

    Defining explicit named subtests

--- internal/server/http_test.go
+++ internal/server/http_test.go
@@ -29,17 +29,18 @@ type fileData struct {
 	FileURL string `json:"fileUrl"`
 }
 
-type unitTest struct {
-	method      string
-	contentType string // Using RFC 6838 standards (MIME types)
-	body        io.Reader
-	httpStatus  int // Expected status
+type httpUnitTest struct {
+	name        string    // Optional name for unit test
+	method      string    // HTTP method
+	contentType string    // Content-Type using RFC 6838 standards (MIME types)
+	body        io.Reader // Request body
+	status      int       // Expected HTTP status
 }
 
 type httpTests struct {
 	endpoint string
 	queryStr string
-	tests    []unitTest
+	tests    []httpUnitTest
 }
 
 // TestDefaultHandler tests if root returns a 403 HTTP status for unsupported HTTP methods.
@@ -50,26 +51,29 @@ func TestDefaultHandler(t *testing.T) {
 	// https://junit.org/junit5/docs/current/user-guide/#writing-tests-parameterized-tests
 	httpTests := httpTests{
 		endpoint: "/",
-		tests: []unitTest{
-			{method: "CONNECT", httpStatus: http.StatusNotFound},
-			{method: "DELETE", httpStatus: http.StatusNotFound},
-			{method: "GET", httpStatus: http.StatusNotFound},
-			{method: "HEAD", httpStatus: http.StatusNotFound},
-			{method: "OPTIONS", httpStatus: http.StatusNotFound},
-			{method: "PATCH", httpStatus: http.StatusNotFound},
-			{method: "POST", httpStatus: http.StatusNotFound},
-			{method: "PUT", httpStatus: http.StatusNotFound},
-			{method: "TRACE", httpStatus: http.StatusNotFound},
+		tests: []httpUnitTest{
+			{method: "CONNECT", status: http.StatusNotFound},
+			{method: "DELETE", status: http.StatusNotFound},
+			{method: "GET", status: http.StatusNotFound},
+			{method: "HEAD", status: http.StatusNotFound},
+			{method: "OPTIONS", status: http.StatusNotFound},
+			{method: "PATCH", status: http.StatusNotFound},
+			{method: "POST", status: http.StatusNotFound},
+			{method: "PUT", status: http.StatusNotFound},
+			{method: "TRACE", status: http.StatusNotFound},
 		},
 	}
 
+	// TODO: This loop can be refactored into its own function to be reused e.g. runTest(httpTests httpTests, handler Handler)
 	for _, test := range httpTests.tests {
-		req, _ := http.NewRequest(test.method, httpTests.endpoint, test.body)
-		w := httptest.NewRecorder()
+		t.Run(test.method, func(t *testing.T) {
+			req, _ := http.NewRequest(test.method, httpTests.endpoint, test.body)
+			w := httptest.NewRecorder()
 
-		defaultHandler(w, req)
+			defaultHandler(w, req)
 
-		assert.Equal(t, http.StatusNotFound, w.Code)
+			assert.Equal(t, http.StatusNotFound, w.Code)
+		})
 	}
 }
 
@@ -77,53 +81,57 @@ func TestDefaultHandler(t *testing.T) {
 func TestDefaultHandlerWithNonRoot(t *testing.T) {
 	httpTests := httpTests{
 		endpoint: "/test",
-		tests: []unitTest{
-			{method: "CONNECT", httpStatus: http.StatusNotFound},
-			{method: "DELETE", httpStatus: http.StatusNotFound},
-			{method: "GET", httpStatus: http.StatusNotFound},
-			{method: "HEAD", httpStatus: http.StatusNotFound},
-			{method: "OPTIONS", httpStatus: http.StatusNotFound},
-			{method: "PATCH", httpStatus: http.StatusNotFound},
-			{method: "POST", httpStatus: http.StatusNotFound},
-			{method: "PUT", httpStatus: http.StatusNotFound},
-			{method: "TRACE", httpStatus: http.StatusNotFound},
+		tests: []httpUnitTest{
+			{method: "CONNECT", status: http.StatusNotFound},
+			{method: "DELETE", status: http.StatusNotFound},
+			{method: "GET", status: http.StatusNotFound},
+			{method: "HEAD", status: http.StatusNotFound},
+			{method: "OPTIONS", status: http.StatusNotFound},
+			{method: "PATCH", status: http.StatusNotFound},
+			{method: "POST", status: http.StatusNotFound},
+			{method: "PUT", status: http.StatusNotFound},
+			{method: "TRACE", status: http.StatusNotFound},
 		},
 	}
 
 	for _, test := range httpTests.tests {
-		req, _ := http.NewRequest(test.method, httpTests.endpoint, test.body)
-		w := httptest.NewRecorder()
+		t.Run(test.method, func(t *testing.T) {
+			req, _ := http.NewRequest(test.method, httpTests.endpoint, test.body)
+			w := httptest.NewRecorder()
 
-		defaultHandler(w, req)
+			defaultHandler(w, req)
 
-		assert.Equal(t, http.StatusNotFound, w.Code)
+			assert.Equal(t, http.StatusNotFound, w.Code)
+		})
 	}
 }
 
-// TestValidateCSVHandler tests non-POST methods to /validate/csv endpoint.
-func TestValidateCSVHandler(t *testing.T) {
+// TestValidateCSVHandlerNonPOST tests non-POST methods to /validate/csv endpoint.
+func TestValidateCSVHandlerNonPOST(t *testing.T) {
 	httpTests := httpTests{
 		endpoint: "/v1/api/validate/csv",
-		tests: []unitTest{
-			{method: "CONNECT", httpStatus: http.StatusMethodNotAllowed},
-			{method: "DELETE", httpStatus: http.StatusMethodNotAllowed},
-			{method: "GET", httpStatus: http.StatusMethodNotAllowed},
-			{method: "HEAD", httpStatus: http.StatusMethodNotAllowed},
-			{method: "OPTIONS", httpStatus: http.StatusMethodNotAllowed},
-			{method: "PATCH", httpStatus: http.StatusMethodNotAllowed},
-			{method: "PUT", httpStatus: http.StatusMethodNotAllowed},
-			{method: "TRACE", httpStatus: http.StatusMethodNotAllowed},
+		tests: []httpUnitTest{
+			{method: "CONNECT", status: http.StatusMethodNotAllowed},
+			{method: "DELETE", status: http.StatusMethodNotAllowed},
+			{method: "GET", status: http.StatusMethodNotAllowed},
+			{method: "HEAD", status: http.StatusMethodNotAllowed},
+			{method: "OPTIONS", status: http.StatusMethodNotAllowed},
+			{method: "PATCH", status: http.StatusMethodNotAllowed},
+			{method: "PUT", status: http.StatusMethodNotAllowed},
+			{method: "TRACE", status: http.StatusMethodNotAllowed},
 		},
 	}
 
 	for _, test := range httpTests.tests {
-		req, _ := http.NewRequest(test.method, httpTests.endpoint, test.body)
+		t.Run(test.method, func(t *testing.T) {
+			req, _ := http.NewRequest(test.method, httpTests.endpoint, test.body)
 
-		w := httptest.NewRecorder()
+			w := httptest.NewRecorder()
 
-		validateCSVHandler(w, req)
+			validateCSVHandler(w, req)
 
-		assert.Equal(t, test.httpStatus, w.Code)
+			assert.Equal(t, test.status, w.Code)
+		})
 	}
 }
 
@@ -131,9 +139,9 @@ func TestValidateCSVHandler(t *testing.T) {
 // will read events off a service bus and use details in those event messages to get the endpoint where file is stored
 // so we can stream it; 2024-06-28 update: API is just for users to test their code against before committing to
 // the actual production pathway which WILL use service bus behind the curtains
-// TestValidateCSVHandlerWithBody tests POST method to /validate/csv endpoint with body.
+// TestValidateCSVHandlerPOST tests POST method to /validate/csv endpoint with body.
 // Note: This test doesn't validate business logic of CSV validation.
-func TestValidateCSVHandlerWithBody(t *testing.T) {
+func TestValidateCSVHandlerPOST(t *testing.T) {
 	csvTestData := "first_name,last_name\nJohn,Doe\nMary,Jane"
 	tsvTestData := "first_name\tlast_name\nJohn\tDoe\nMary\tJane"
 	jsonTestData := `{
@@ -153,40 +161,49 @@ func TestValidateCSVHandlerWithBody(t *testing.T) {
 
 	httpTests := httpTests{
 		endpoint: "/v1/api/validate/csv",
-		tests: []unitTest{
-			// Missing file in body
-			{method: "POST", httpStatus: http.StatusBadRequest,
+		tests: []httpUnitTest{
+			{name: "Missing file in body", method: "POST",
+				status:      http.StatusBadRequest,
 				contentType: "text/csv"},
-			// Non-CSV file in body
-			{method: "POST", httpStatus: http.StatusBadRequest,
-				contentType: "application/json", body: jsonReader},
-			// CSV file in body but wrong content type (JSON)
-			{method: "POST", httpStatus: http.StatusBadRequest,
-				contentType: "application/json", body: csvReader},
-			// CSV file in body but wrong content type (Excel, CSV adjacent format)
-			{method: "POST", httpStatus: http.StatusBadRequest,
-				contentType: "application/vnd.ms-excel", body: csvReader},
-			// CSV file in body but accepted file format for processing (any format with delimiter-separated values)
-			{method: "POST", httpStatus: http.StatusAccepted,
-				contentType: "text/tab-separated-values", body: csvReader},
-			// TSV file in body
-			{method: "POST", httpStatus: http.StatusAccepted,
-				contentType: "text/tab-separated-values", body: tsvReader},
-			// CSV file in body
-			{method: "POST", httpStatus: http.StatusAccepted,
-				contentType: "text/csv", body: csvReader},
+			{name: "Non-CSV file in body", method: "POST",
+				status:      http.StatusBadRequest,
+				contentType: "application/json",
+				body:        jsonReader},
+			{name: "CSV file in body but wrong content type (JSON)", method: "POST",
+				status:      http.StatusBadRequest,
+				contentType: "application/json",
+				body:        csvReader},
+			{name: "CSV file in body but wrong content type (Excel, CSV adjacent format)", method: "POST",
+				status:      http.StatusBadRequest,
+				contentType: "application/vnd.ms-excel",
+				body:        csvReader},
+			{name: "CSV file in body but accepted file format for processing (any format with delimiter-separated values)",
+				method:      "POST",
+				status:      http.StatusAccepted,
+				contentType: "text/tab-separated-values",
+				body:        csvReader},
+			{name: "TSV file in body", method: "POST",
+				status:      http.StatusAccepted,
+				contentType: "text/tab-separated-values",
+				body:        tsvReader},
+			{name: "CSV file in body", method: "POST",
+				status:      http.StatusAccepted,
+				contentType: "text/csv",
+				body:        csvReader},
 		},
 	}
 
 	for _, test := range httpTests.tests {
-		req, _ := http.NewRequest(test.method, httpTests.endpoint, test.body)
-		req.Header.Set("Content-Type", test.contentType)
+		t.Run(test.name, func(t *testing.T) {
+			req, _ := http.NewRequest(test.method, httpTests.endpoint, test.body)
+			req.Header.Set("Content-Type", test.contentType)
 
-		w := httptest.NewRecorder()
+			w := httptest.NewRecorder()
 
-		validateCSVHandler(w, req)
+			validateCSVHandler(w, req)
 
-		assert.Equal(t, test.httpStatus, w.Code)
+			assert.Equal(t, test.status, w.Code)
+		})
 	}
 }
 
@@ -213,21 +230,23 @@ func TestValidateCSVHandlerEventMessage(t *testing.T) {
 
 	httpTests := httpTests{
 		endpoint: "/v1/api/validate/csv",
-		tests: []unitTest{
-			{method: "POST", httpStatus: http.StatusBadRequest,
+		tests: []httpUnitTest{
+			{method: "POST", status: http.StatusBadRequest,
 				contentType: "application/json", body: jsonReader},
 		},
 	}
 
 	for _, test := range httpTests.tests {
-		req, _ := http.NewRequest(test.method, httpTests.endpoint, test.body)
-		req.Header.Set("Content-Type", test.contentType)
+		t.Run("", func(t *testing.T) {
+			req, _ := http.NewRequest(test.method, httpTests.endpoint, test.body)
+			req.Header.Set("Content-Type", test.contentType)
 
-		w := httptest.NewRecorder()
+			w := httptest.NewRecorder()
 
-		validateCSVHandler(w, req)
+			validateCSVHandler(w, req)
 
-		assert.Equal(t, test.httpStatus, w.Code)
+			assert.Equal(t, test.status, w.Code)
+		})
 	}
 }
 
@@ -235,32 +254,35 @@ func TestValidateCSVHandlerEventMessage(t *testing.T) {
 func TestHealthCheckHandler(t *testing.T) {
 	httpTests := httpTests{
 		endpoint: "/v1/api/health",
-		tests: []unitTest{
-			{method: "CONNECT", httpStatus: http.StatusMethodNotAllowed},
-			{method: "DELETE", httpStatus: http.StatusMethodNotAllowed},
-			{method: "GET", httpStatus: http.StatusOK},
-			{method: "HEAD", httpStatus: http.StatusMethodNotAllowed},
-			{method: "OPTIONS", httpStatus: http.StatusMethodNotAllowed},
-			{method: "PATCH", httpStatus: http.StatusMethodNotAllowed},
-			{method: "POST", httpStatus: http.StatusMethodNotAllowed},
-			{method: "PUT", httpStatus: http.StatusMethodNotAllowed},
-			{method: "TRACE", httpStatus: http.StatusMethodNotAllowed},
+		tests: []httpUnitTest{
+			{method: "CONNECT", status: http.StatusMethodNotAllowed},
+			{method: "DELETE", status: http.StatusMethodNotAllowed},
+			{method: "GET", status: http.StatusOK},
+			{method: "HEAD", status: http.StatusMethodNotAllowed},
+			{method: "OPTIONS", status: http.StatusMethodNotAllowed},
+			{method: "PATCH", status: http.StatusMethodNotAllowed},
+			{method: "POST", status: http.StatusMethodNotAllowed},
+			{method: "PUT", status: http.StatusMethodNotAllowed},
+			{method: "TRACE", status: http.StatusMethodNotAllowed},
 		},
 	}
 
 	for _, test := range httpTests.tests {
-		req, _ := http.NewRequest(test.method, httpTests.endpoint, test.body)
-		w := httptest.NewRecorder()
+		t.Run(test.method, func(t *testing.T) {
+			req, _ := http.NewRequest(test.method, httpTests.endpoint, test.body)
+			w := httptest.NewRecorder()
 
-		healthCheckHandler(w, req)
+			healthCheckHandler(w, req)
 
-		assert.Equal(t, test.httpStatus, w.Code)
+			assert.Equal(t, test.status, w.Code)
+		})
 	}
 }
 
 // TestDuplicateServer confirms if only one HTTP server can be active at any time
 func TestDuplicateServer(t *testing.T) {
 	// TODO: Implement this
+	// TODO: This test may need to be in its own separate package for black-box testing
 	_ = New()
 	assert.NotNil(t, New())
 }
