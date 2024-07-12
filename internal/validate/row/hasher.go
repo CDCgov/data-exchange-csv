d7791f2e10d731b55ea4610f978f5b28ee37a00e
TeaSmith7 <137535421+TeaSmith7@users.noreply.github.com>
2024-07-12 14:13:44 -0400

    Encoding hash value,  returned by sha256 hash function to base64-encoded string implemented unit tests under hasher_test.go

--- internal/validate/row/hasher.go
+++ internal/validate/row/hasher.go
@@ -2,20 +2,23 @@ package row
 
 import (
 	"crypto/sha256"
+	"encoding/base64"
 	"strings"
 
 	"github.com/CDCgov/data-exchange-csv/cmd/internal/constants"
 )
 
-func ComputeHash(row []string, delimiter string) [32]byte {
-	separator := string(constants.CSV)
+func ComputeHash(row []string, delimiter string) string {
+	separator := string(constants.COMMA)
 
 	if delimiter == constants.TSV {
-		separator = string(constants.TSV)
+		separator = string(constants.TAB)
 	}
+	concatenatedRow := strings.Join(row, separator)
+	hash := sha256.Sum256([]byte(concatenatedRow))
 
-	contatenatedRow := strings.Join(row, separator)
-	hash := sha256.Sum256([]byte(contatenatedRow))
+	//we need to convert [32]byte to []byte before using base64.StdEncoding.EncodeToString()
+	hashSlice := hash[:]
 
-	return hash
+	return base64.StdEncoding.EncodeToString(hashSlice)
 }
---
+++ internal/validate/row/hasher_test.go
@@ -0,0 +1,45 @@
+package row
+
+import (
+	"crypto/sha256"
+	"encoding/base64"
+	"testing"
+
+	"github.com/CDCgov/data-exchange-csv/cmd/internal/constants"
+)
+
+func computeExpectedHash(concatenatedRow string) string {
+	hash := sha256.Sum256([]byte(concatenatedRow))
+	hashSlice := hash[:]
+
+	return base64.StdEncoding.EncodeToString(hashSlice)
+}
+
+func TestComputeHash(t *testing.T) {
+	tests := []struct {
+		name      string
+		row       []string
+		delimiter string
+		expected  string
+	}{
+		{
+			name:      "CSV file with the valid input",
+			row:       []string{"George", "30", "Engineer"},
+			delimiter: constants.CSV,
+			expected:  computeExpectedHash("George,30,Engineer"),
+		},
+		{
+			name:      "TSV file with the valid input",
+			row:       []string{"Bob", "25", "Doctor"},
+			delimiter: constants.TSV,
+			expected:  computeExpectedHash("Bob\t25\tDoctor"),
+		},
+	}
+	for _, test := range tests {
+		result := ComputeHash(test.row, test.delimiter)
+		if result != test.expected {
+			t.Errorf("ActualHash(%v, %q) = %q; ExpectedHash %q", test.row, test.delimiter, result, test.expected)
+		}
+	}
+
+}
