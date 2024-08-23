dc460c87475da82c3f35966591cb0c762523e352
TeaSmith7 <137535421+TeaSmith7@users.noreply.github.com>
2024-08-23 15:00:13 -0400

    Initial work setting up default logger with slog

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,36 @@
 package main
 
 import (
+	"log/slog"
+	"os"
+
 	"github.com/CDCgov/data-exchange-csv/cmd/internal/constants"
 	"github.com/CDCgov/data-exchange-csv/cmd/internal/models"
 	"github.com/CDCgov/data-exchange-csv/cmd/internal/processor"
 	"github.com/CDCgov/data-exchange-csv/cmd/internal/validate/file"
 	"github.com/CDCgov/data-exchange-csv/cmd/internal/validate/row"
+	"github.com/CDCgov/data-exchange-csv/cmd/pkg/sloger"
 )
 
+func init() {
+	logFile, err := os.OpenFile("logs/csv-validation.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND|os.O_TRUNC, 0666)
+	if err != nil {
+		panic("Failed to Open log file:" + logFile.Name())
+	}
+
+	//create a new logger with
+	logger := slog.New(slog.NewJSONHandler(logFile, &slog.HandlerOptions{Level: slog.LevelInfo}))
+
+	//set the logger as default logger
+	sloger.SetDefaultLogger(logger)
+
+}
 func main() {
+
 	event := "data/event_source.json"
+	logger := sloger.With("event", "main")
+
+	logger.Info("Application Started")
 
 	fileValidationResult := file.Validate(event)
 	processor.ProcessFileValidationResult(fileValidationResult)
