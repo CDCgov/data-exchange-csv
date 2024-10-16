7ead4b64a5d0ef9afcb760a0c4312a2a90107a2b
TeaSmith7 <137535421+TeaSmith7@users.noreply.github.com>
2024-10-16 17:18:24 -0400

    updated TestRowToJsonNoHeader test case there are lot of similarities between the two test cases, will look into how to combine them

--- internal/transform/row_to_json_test.go
+++ internal/transform/row_to_json_test.go
@@ -69,29 +69,28 @@ func TestRowToJsonSuccess(t *testing.T) {
 	}
 
 	//Unmarshall expected JSON row for comparison
-	var jsonMap map[string]interface{}
-	err = json.Unmarshal(expectedResult.JsonRow, &jsonMap)
+	var jsonRowMap map[string]interface{}
+	err = json.Unmarshal(expectedResult.JsonRow, &jsonRowMap)
 	if err != nil {
 		t.Errorf("Error unmarshalling expectedResult.JsonRow %s", err)
 	}
 
 	//validate transformed JSON row
-	if !reflect.DeepEqual(jsonMap, actualTransformedResult["json_row"]) {
-		t.Errorf("Expected jsonRow  %v and actual  %v do not match", jsonMap, actualTransformedResult["json_row"])
+	if !reflect.DeepEqual(jsonRowMap, actualTransformedResult["json_row"]) {
+		t.Errorf("Expected jsonRow  %v and actual  %v do not match", jsonRowMap, actualTransformedResult["json_row"])
 	}
 
 }
 
-/*
 func TestRowToJsonNoHeader(t *testing.T) {
-	mockSender := &MockSendEventsToDestination{}
+	mockSender := &MockTransformedResult{}
 
+	//initialize input parameters for `RowToJson``
 	rowToBeTransformed := []string{"Doppio", "Cortado", "Galao", "Lungo"}
-	params := models.FileValidationParams{
-		FileUUID: uuid.New(),
-		Header:   []string{},
+	params := models.FileValidationResult{
+		FileUUID:  uuid.New(),
+		HasHeader: false,
 	}
-
 	rowUUID := uuid.New()
 
 	expectedTransformedRow, _ := json.Marshal(map[string]string{"0": "Doppio", "1": "Cortado", "2": "Galao", "3": "Lungo"})
@@ -102,15 +101,43 @@ func TestRowToJsonNoHeader(t *testing.T) {
 		Status:   constants.STATUS_SUCCESS,
 		JsonRow:  expectedTransformedRow,
 	}
+	//call the function being tested
+	RowToJson(rowToBeTransformed, params, rowUUID, false, nil, mockSender.callback)
 
-	RowToJson(rowToBeTransformed, params, rowUUID, mockSender.callback)
-
-	if mockSender.destination != constants.TRANSFORMED_ROW_REPORTS {
-		t.Errorf("Expected destination  %v and actual  %v do not match", constants.TRANSFORMED_ROW_REPORTS, mockSender.destination)
+	//deserialize the transformed result to a map
+	var actualTransformedResult map[string]interface{}
+	//convert transformed result to slice of bytes before Unmarshalling
+	convertTransformedResultToByteSlice := []byte(mockSender.transformedResult)
+	err := json.Unmarshal(convertTransformedResultToByteSlice, &actualTransformedResult)
+	if err != nil {
+		t.Errorf("Unexpected error occurred during JSON Unmarshalling %v", err.Error())
 	}
 
-	if !compareTransformationResult(expectedResult, mockSender.result.(models.RowTransformationResult)) {
-		t.Errorf("Expected transformation result %v and actual  %v do not match", expectedResult, mockSender.result)
+	//validate File UUID
+	fileUUIDAsString := expectedResult.FileUUID.String()
+	if fileUUIDAsString != actualTransformedResult["file_uuid"] {
+		t.Errorf("Expected File UUID  %v and actual  %v do not match", fileUUIDAsString, actualTransformedResult["file_uuid"])
 	}
+	//validate  Row UUID
+	rowUUIDAsString := expectedResult.RowUUID.String()
+	if rowUUIDAsString != actualTransformedResult["row_uuid"] {
+		t.Errorf("Expected Row UUID  %v and actual  %v do not match", rowUUIDAsString, actualTransformedResult["row_uuid"])
+	}
+	//Validate Status
+	if expectedResult.Status != actualTransformedResult["status"] {
+		t.Errorf("Expected status  %v and actual  %v do not match", expectedResult.Status, actualTransformedResult["status"])
+	}
+
+	//Unmarshall expected JSON row for comparison
+	var jsonRowMap map[string]interface{}
+	err = json.Unmarshal(expectedResult.JsonRow, &jsonRowMap)
+	if err != nil {
+		t.Errorf("Error unmarshalling expectedResult.JsonRow %s", err)
+	}
+
+	//validate transformed JSON row
+	if !reflect.DeepEqual(jsonRowMap, actualTransformedResult["json_row"]) {
+		t.Errorf("Expected jsonRow  %v and actual  %v do not match", jsonRowMap, actualTransformedResult["json_row"])
+	}
+
 }
-*/
