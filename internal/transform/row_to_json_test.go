cf37ff5c445c622b5ea371b0a33cbad6d6efbff7
TeaSmith7 <137535421+TeaSmith7@users.noreply.github.com>
2024-10-16 17:10:28 -0400

    changes to ow_to_json_test.go >> updated TestRowToJsonSuccess with the new changes to transformation logic >> renamed structs to improve readability and delete functions that were no longer in use

--- internal/transform/row_to_json_test.go
+++ internal/transform/row_to_json_test.go
@@ -1,8 +1,8 @@
 package transform
 
-/*
 import (
 	"encoding/json"
+	"reflect"
 	"testing"
 
 	"github.com/CDCgov/data-exchange-csv/cmd/internal/constants"
@@ -10,42 +10,27 @@ import (
 	"github.com/google/uuid"
 )
 
-type MockSendEventsToDestination struct {
-	result      interface{}
-	destination string
+type MockTransformedResult struct {
+	transformedResult string
 }
 
-func (m *MockSendEventsToDestination) callback(result interface{}, destination string) {
-	m.result = result
-	m.destination = destination
-}
+// callback will store the transformation result from RowToJson
+func (m *MockTransformedResult) callback(params models.RowCallbackParams) error {
+	m.transformedResult = params.TransformationResult.(string)
 
-func compareTransformationResult(expected, actual models.RowTransformationResult) bool {
-	if actual.FileUUID != expected.FileUUID || actual.RowUUID != expected.RowUUID || actual.Status != expected.Status {
-		return false
-	}
-	if (actual.JsonRow == nil && expected.JsonRow != nil) || (actual.JsonRow != nil && expected.JsonRow == nil) {
-		return false
-	}
-	if string(actual.JsonRow) != string(expected.JsonRow) {
-		return false
-	}
-	if (actual.Error == nil && expected.Error != nil) || (actual.Error != nil && expected.Error == nil) {
-		return false
-	}
-
-	return true
+	return nil
 }
 
 func TestRowToJsonSuccess(t *testing.T) {
-	mockSender := &MockSendEventsToDestination{}
+	mockSender := &MockTransformedResult{}
 
+	//initialize input parameters for `RowToJson``
 	rowToBeTransformed := []string{"Doppio", "Cortado", "Galao", "Lungo"}
-	params := models.FileValidationParams{
-		FileUUID: uuid.New(),
-		Header:   []string{"Espresso", "Balanced Espresso", "Latte", "Long-pull Espresso"},
+	header := []string{"Espresso", "Balanced Espresso", "Latte", "Long-pull Espresso"}
+	params := models.FileValidationResult{
+		FileUUID:  uuid.New(),
+		HasHeader: true,
 	}
-
 	rowUUID := uuid.New()
 
 	expectedTransformedRow, _ := json.Marshal(map[string]string{"Espresso": "Doppio", "Balanced Espresso": "Cortado", "Latte": "Galao", "Long-pull Espresso": "Lungo"})
@@ -56,17 +41,48 @@ func TestRowToJsonSuccess(t *testing.T) {
 		Status:   constants.STATUS_SUCCESS,
 		JsonRow:  expectedTransformedRow,
 	}
+	//call the function being tested
+	RowToJson(rowToBeTransformed, params, rowUUID, false, header, mockSender.callback)
 
-	RowToJson(rowToBeTransformed, params, rowUUID, mockSender.callback)
+	//deserialize the transformed result to a map
+	var actualTransformedResult map[string]interface{}
+	//convert transformed result to slice of bytes before Unmarshalling
+	convertTransformedResultToByteSlice := []byte(mockSender.transformedResult)
+	err := json.Unmarshal(convertTransformedResultToByteSlice, &actualTransformedResult)
+	if err != nil {
+		t.Errorf("Unexpected error occurred during JSON Unmarshalling %v", err.Error())
+	}
 
-	if mockSender.destination != constants.TRANSFORMED_ROW_REPORTS {
-		t.Errorf("Expected destination  %v and actual  %v do not match", constants.TRANSFORMED_ROW_REPORTS, mockSender.destination)
+	//validate File UUID
+	fileUUIDAsString := expectedResult.FileUUID.String()
+	if fileUUIDAsString != actualTransformedResult["file_uuid"] {
+		t.Errorf("Expected File UUID  %v and actual  %v do not match", fileUUIDAsString, actualTransformedResult["file_uuid"])
 	}
-	if !compareTransformationResult(expectedResult, mockSender.result.(models.RowTransformationResult)) {
-		t.Errorf("Expected transformation result %v and actual  %v do not match", expectedResult, mockSender.result)
+	//validate  Row UUID
+	rowUUIDAsString := expectedResult.RowUUID.String()
+	if rowUUIDAsString != actualTransformedResult["row_uuid"] {
+		t.Errorf("Expected Row UUID  %v and actual  %v do not match", rowUUIDAsString, actualTransformedResult["row_uuid"])
 	}
+	//Validate Status
+	if expectedResult.Status != actualTransformedResult["status"] {
+		t.Errorf("Expected status  %v and actual  %v do not match", expectedResult.Status, actualTransformedResult["status"])
+	}
+
+	//Unmarshall expected JSON row for comparison
+	var jsonMap map[string]interface{}
+	err = json.Unmarshal(expectedResult.JsonRow, &jsonMap)
+	if err != nil {
+		t.Errorf("Error unmarshalling expectedResult.JsonRow %s", err)
+	}
+
+	//validate transformed JSON row
+	if !reflect.DeepEqual(jsonMap, actualTransformedResult["json_row"]) {
+		t.Errorf("Expected jsonRow  %v and actual  %v do not match", jsonMap, actualTransformedResult["json_row"])
+	}
+
 }
 
+/*
 func TestRowToJsonNoHeader(t *testing.T) {
 	mockSender := &MockSendEventsToDestination{}
 
