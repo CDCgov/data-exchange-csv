7523ddf27db5de48bddf727b26156e177e2d7aef
TeaSmith7 <137535421+TeaSmith7@users.noreply.github.com>
2024-10-17 10:17:47 -0400

    Created private alidate() function to be shared across test cases

--- internal/transform/row_to_json_test.go
+++ internal/transform/row_to_json_test.go
@@ -14,17 +14,48 @@ type MockTransformedResult struct {
 	transformedResult string
 }
 
-// callback will store the transformation result from RowToJson
 func (m *MockTransformedResult) callback(params models.RowCallbackParams) error {
 	m.transformedResult = params.TransformationResult.(string)
-
 	return nil
+}
+
+func validate(t *testing.T, expectedResult models.RowTransformationResult, actualTransformedResult string) {
+	var actualResultMap map[string]interface{}
+	convertTransformedResultToByteSlice := []byte(actualTransformedResult)
+	err := json.Unmarshal(convertTransformedResultToByteSlice, &actualResultMap)
+	if err != nil {
+		t.Errorf("Unexpected error occurred during JSON Unmarshalling: %v", err.Error())
+	}
+
+	// Validate File UUID
+	if expectedResult.FileUUID.String() != actualResultMap["file_uuid"] {
+		t.Errorf("Expected File UUID %v and actual %v do not match", expectedResult.FileUUID.String(), actualResultMap["file_uuid"])
+	}
+	// Validate Row UUID
+	if expectedResult.RowUUID.String() != actualResultMap["row_uuid"] {
+		t.Errorf("Expected Row UUID %v and actual %v do not match", expectedResult.RowUUID.String(), actualResultMap["row_uuid"])
+	}
+	// Validate Status
+	if expectedResult.Status != actualResultMap["status"] {
+		t.Errorf("Expected status %v and actual %v do not match", expectedResult.Status, actualResultMap["status"])
+	}
+
+	// Unmarshal expected JSON row for comparison
+	var expectedJsonRowMap map[string]interface{}
+	err = json.Unmarshal(expectedResult.JsonRow, &expectedJsonRowMap)
+	if err != nil {
+		t.Errorf("Error unmarshalling expectedResult.JsonRow: %s", err)
+	}
+
+	// Validate transformed JSON row
+	if !reflect.DeepEqual(expectedJsonRowMap, actualResultMap["json_row"]) {
+		t.Errorf("Expected jsonRow %v and actual %v do not match", expectedJsonRowMap, actualResultMap["json_row"])
+	}
 }
 
 func TestRowToJsonSuccess(t *testing.T) {
 	mockSender := &MockTransformedResult{}
 
-	//initialize input parameters for `RowToJson``
 	rowToBeTransformed := []string{"Doppio", "Cortado", "Galao", "Lungo"}
 	header := []string{"Espresso", "Balanced Espresso", "Latte", "Long-pull Espresso"}
 	params := models.FileValidationResult{
@@ -34,58 +65,20 @@ func TestRowToJsonSuccess(t *testing.T) {
 	rowUUID := uuid.New()
 
 	expectedTransformedRow, _ := json.Marshal(map[string]string{"Espresso": "Doppio", "Balanced Espresso": "Cortado", "Latte": "Galao", "Long-pull Espresso": "Lungo"})
-
 	expectedResult := models.RowTransformationResult{
 		FileUUID: params.FileUUID,
 		RowUUID:  rowUUID,
 		Status:   constants.STATUS_SUCCESS,
 		JsonRow:  expectedTransformedRow,
 	}
-	//call the function being tested
+
 	RowToJson(rowToBeTransformed, params, rowUUID, false, header, mockSender.callback)
-
-	//deserialize the transformed result to a map
-	var actualTransformedResult map[string]interface{}
-	//convert transformed result to slice of bytes before Unmarshalling
-	convertTransformedResultToByteSlice := []byte(mockSender.transformedResult)
-	err := json.Unmarshal(convertTransformedResultToByteSlice, &actualTransformedResult)
-	if err != nil {
-		t.Errorf("Unexpected error occurred during JSON Unmarshalling %v", err.Error())
-	}
-
-	//validate File UUID
-	fileUUIDAsString := expectedResult.FileUUID.String()
-	if fileUUIDAsString != actualTransformedResult["file_uuid"] {
-		t.Errorf("Expected File UUID  %v and actual  %v do not match", fileUUIDAsString, actualTransformedResult["file_uuid"])
-	}
-	//validate  Row UUID
-	rowUUIDAsString := expectedResult.RowUUID.String()
-	if rowUUIDAsString != actualTransformedResult["row_uuid"] {
-		t.Errorf("Expected Row UUID  %v and actual  %v do not match", rowUUIDAsString, actualTransformedResult["row_uuid"])
-	}
-	//Validate Status
-	if expectedResult.Status != actualTransformedResult["status"] {
-		t.Errorf("Expected status  %v and actual  %v do not match", expectedResult.Status, actualTransformedResult["status"])
-	}
-
-	//Unmarshall expected JSON row for comparison
-	var jsonRowMap map[string]interface{}
-	err = json.Unmarshal(expectedResult.JsonRow, &jsonRowMap)
-	if err != nil {
-		t.Errorf("Error unmarshalling expectedResult.JsonRow %s", err)
-	}
-
-	//validate transformed JSON row
-	if !reflect.DeepEqual(jsonRowMap, actualTransformedResult["json_row"]) {
-		t.Errorf("Expected jsonRow  %v and actual  %v do not match", jsonRowMap, actualTransformedResult["json_row"])
-	}
-
+	validate(t, expectedResult, mockSender.transformedResult)
 }
 
 func TestRowToJsonNoHeader(t *testing.T) {
 	mockSender := &MockTransformedResult{}
 
-	//initialize input parameters for `RowToJson``
 	rowToBeTransformed := []string{"Doppio", "Cortado", "Galao", "Lungo"}
 	params := models.FileValidationResult{
 		FileUUID:  uuid.New(),
@@ -94,50 +87,13 @@ func TestRowToJsonNoHeader(t *testing.T) {
 	rowUUID := uuid.New()
 
 	expectedTransformedRow, _ := json.Marshal(map[string]string{"0": "Doppio", "1": "Cortado", "2": "Galao", "3": "Lungo"})
-
 	expectedResult := models.RowTransformationResult{
 		FileUUID: params.FileUUID,
 		RowUUID:  rowUUID,
 		Status:   constants.STATUS_SUCCESS,
 		JsonRow:  expectedTransformedRow,
 	}
-	//call the function being tested
+
 	RowToJson(rowToBeTransformed, params, rowUUID, false, nil, mockSender.callback)
-
-	//deserialize the transformed result to a map
-	var actualTransformedResult map[string]interface{}
-	//convert transformed result to slice of bytes before Unmarshalling
-	convertTransformedResultToByteSlice := []byte(mockSender.transformedResult)
-	err := json.Unmarshal(convertTransformedResultToByteSlice, &actualTransformedResult)
-	if err != nil {
-		t.Errorf("Unexpected error occurred during JSON Unmarshalling %v", err.Error())
-	}
-
-	//validate File UUID
-	fileUUIDAsString := expectedResult.FileUUID.String()
-	if fileUUIDAsString != actualTransformedResult["file_uuid"] {
-		t.Errorf("Expected File UUID  %v and actual  %v do not match", fileUUIDAsString, actualTransformedResult["file_uuid"])
-	}
-	//validate  Row UUID
-	rowUUIDAsString := expectedResult.RowUUID.String()
-	if rowUUIDAsString != actualTransformedResult["row_uuid"] {
-		t.Errorf("Expected Row UUID  %v and actual  %v do not match", rowUUIDAsString, actualTransformedResult["row_uuid"])
-	}
-	//Validate Status
-	if expectedResult.Status != actualTransformedResult["status"] {
-		t.Errorf("Expected status  %v and actual  %v do not match", expectedResult.Status, actualTransformedResult["status"])
-	}
-
-	//Unmarshall expected JSON row for comparison
-	var jsonRowMap map[string]interface{}
-	err = json.Unmarshal(expectedResult.JsonRow, &jsonRowMap)
-	if err != nil {
-		t.Errorf("Error unmarshalling expectedResult.JsonRow %s", err)
-	}
-
-	//validate transformed JSON row
-	if !reflect.DeepEqual(jsonRowMap, actualTransformedResult["json_row"]) {
-		t.Errorf("Expected jsonRow  %v and actual  %v do not match", jsonRowMap, actualTransformedResult["json_row"])
-	}
-
+	validate(t, expectedResult, mockSender.transformedResult)
 }
